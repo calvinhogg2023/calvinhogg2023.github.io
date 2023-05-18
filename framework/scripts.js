5887f23f4adb822c41e6a2765182918c6764f7d8
calvinhogg2023 <131737842+calvinhogg2023@users.noreply.github.com>
2023-05-18 19:08:51 +1200

    update

--- framework/scripts.js
+++ framework/scripts.js
@@ -7,27 +7,6 @@ window.addEventListener('load', () => {
 history.scrollRestoration = 'auto';
 
 
-
-
-
-var element1 = document.getElementById("projectBackground");
-var element2 = document.getElementById("projectContent");
-
-$('.modal').on('show.bs.modal', function (e) {
-	element1.classList.add("modal-greyscale");
-	element2.classList.add("modal-greyscale");
-})
-
-$('.modal').on('hide.bs.modal', function (e) {
-	element1.classList.remove("modal-greyscale");
-	element2.classList.remove("modal-greyscale");
-})
-
-
-
-
-
-
 const html = document.documentElement;
 const canvas = document.getElementById("hero-lightpass");
 const context = canvas.getContext("2d");
