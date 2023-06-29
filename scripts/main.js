4b9526e0fb6f214f84a6fb2351467df4743c3b53
calvinhogg2023 <131737842+calvinhogg2023@users.noreply.github.com>
2023-06-29 14:33:09 +1200

    update

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,33 @@
 history.scrollRestoration = 'auto';
+
+function toggleMenu() {
+	var x = document.getElementById("menu");
+	var y = document.getElementById("navbar-menuButton");
+	if (x.style.height === "300px") {
+		x.style.height = "0px";
+		x.style.boxShadow = "var(--shadow-none)";
+		y.style.opacity = "50%";
+	} else {
+		x.style.height = "300px";
+		x.style.boxShadow = "var(--shadow-small)";
+		y.style.opacity = "100%";
+	}
+}
+
+function hideMenu() {
+	var x = document.getElementById("menu");
+	var y = document.getElementById("navbar-menuButton");
+	x.style.height = "0px";
+	x.style.boxShadow = "var(--shadow-none)";
+	y.style.opacity = "50%";
+}
+
+function menuButtonHover() {
+	var y = document.getElementById("navbar-menuButton");
+	y.classList.add("hovered");
+}
+
+function menuButtonNotHover() {
+	var y = document.getElementById("navbar-menuButton");
+	y.classList.remove("hovered");
+}
